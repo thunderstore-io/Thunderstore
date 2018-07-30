1f648d848b418a372e9ed56cb7aa48c360e45637
Mythic <mythicmaniac@gmail.com>
2018-07-30 19:11:49 +0300

    Fix logs

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -it --rm --name certbot \
   -v nexus_certs:/etc/letsencrypt \
-  -v nexus_logs:/var/log \
+  -v nexus_logs:/var/log/letsencrypt \
   -v nexus_letsencrypt:/var/www/.well-known \
   quay.io/letsencrypt/letsencrypt -t certonly \
   --agree-tos --renew-by-default \
