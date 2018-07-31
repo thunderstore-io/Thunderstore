90d8e1737acdf6b4162ba43ad6b44d552e5f2ae9
Mythic <mythicmaniac@gmail.com>
2018-07-31 04:38:34 +0300

    Replace Nginx with Caddy
    Caddy should work better for our purposes, so use that instead
    of Nginx

--- nginx/root/reload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-envsubst '$SERVER_NAME' \
-< /etc/nginx/conf.d/app.template \
-> /etc/nginx/conf.d/app.conf
--- renew_certs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run -it --rm --name certbot \
-  -v nexus_certs:/etc/letsencrypt \
-  -v nexus_logs:/var/log/letsencrypt \
-  -v nexus_letsencrypt:/var/www/.well-known \
-  quay.io/letsencrypt/letsencrypt -t certonly \
-  --agree-tos --renew-by-default \
-  --webroot -w /var/www \
-  -d nexus.leaguesandbox.gg
