7f213b5a0c55132fed5cc627aa2a3cdf857fbb46
Mythic <mythicmaniac@gmail.com>
2019-05-08 20:46:24 +0300

    Make dynamic HTML objects be fixed to a single placement
    Add a "placement" field on dynamic HTML objects and merge the two
    content fields into a single one

--- django/frontend/models.py
+++ django/frontend/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
+from core.utils import ChoiceEnum
+
+
+class DynamicPlacement(ChoiceEnum):
+    html_head = "html_head"
+    html_body_beginning = "html_body_beginning"
+    content_beginning = "content_beginning"
+    content_end = "content_end"
+
 
 class DynamicHTML(models.Model):
-    name = models.CharField(max_length=256)
-    head_content = models.TextField(null=True, blank=True)
-    body_content = models.TextField(null=True, blank=True)
+    name = models.CharField(
+        max_length=256,
+    )
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+    placement = models.CharField(
+        max_length=256,
+        db_index=True,
+        choices=DynamicPlacement.as_choices(),
+    )
 
     is_active = models.BooleanField(
         default=True,
