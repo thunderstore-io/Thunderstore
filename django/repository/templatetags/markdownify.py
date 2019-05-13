199ca0a4293a782259175dd47bd8374240f7def5
Mythic <mythicmaniac@gmail.com>
2019-05-13 22:46:37 +0300

    Improve markdown rendering
    Improve the current markdown rendering by introducing more
    github-like styling.

--- django/repository/templatetags/markdownify.py
+++ django/repository/templatetags/markdownify.py
@@ -9,12 +9,14 @@ register = template.Library()
 
 
 def deduplicate_escape(text):
-    return (text
-            .replace("&amp;lt;", "&lt;")
-            .replace("&amp;gt;", "&gt;")
-            .replace("&amp;quot;", "&quot;")
-            .replace("&amp;#39;", "&#39;")
-            )
+    return (
+        text
+        .replace("&amp;lt;", "&lt;")
+        .replace("&amp;gt;", "&gt;")
+        .replace("&amp;quot;", "&quot;")
+        .replace("&amp;#39;", "&#39;")
+        .replace("&amp;amp;", "&amp;")
+    )
 
 
 @register.filter
@@ -23,7 +25,19 @@ def markdownify(value):
     return mark_safe(deduplicate_escape(markdown.markdown(
         escape(value),
         extensions=[
+            "markdown.extensions.abbr",
+            "markdown.extensions.def_list",
             "markdown.extensions.fenced_code",
+            "markdown.extensions.footnotes",
             "markdown.extensions.tables",
+            "markdown.extensions.admonition",
+            # "markdown.extensions.codehilite",  # TODO: Configure
+            "markdown.extensions.nl2br",
+            "markdown.extensions.sane_lists",
+            "markdown.extensions.toc",
+            "markdown.extensions.wikilinks",
+
+            "pymdownx.magiclink",
+            "pymdownx.tilde",
         ]
     )))
