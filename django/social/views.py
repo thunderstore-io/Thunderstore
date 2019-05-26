679118a50fa0600e0af53475ba7071f421b7bde0
Mythic <mythicmaniac@gmail.com>
2019-05-26 22:02:30 +0300

    Add oauth disconnect support
    Support disconnection of oauth accounts

--- django/social/views.py
+++ django/social/views.py
@@ -1,14 +1,32 @@
-from django.conf import settings
-from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django import forms
 
 
-class LinkedAccountsView(DetailView):
-    model = settings.AUTH_USER_MODEL
-    template_name = "social/profile_detail.html"
+class LinkedAccountDisconnectForm(forms.Form):
+    provider = forms.CharField()
 
-    def get_object(self, *args, **kwargs):
-        return self.request.user
+
+class LinkedAccountsView(FormView):
+    template_name = "settings/linked_accounts.html"
+    form_class = LinkedAccountDisconnectForm
+    success_url = reverse_lazy("settings.linked-accounts")
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
+        context["can_disconnect"] = self.can_disconnect
         return context
+
+    @property
+    def can_disconnect(self):
+        return self.request.user.social_auth.count() > 1
+
+    def disconnect_account(self, provider):
+        if not self.can_disconnect:
+            return
+        social_auth = self.request.user.social_auth.filter(provider=provider).first()
+        social_auth.delete()
+
+    def form_valid(self, form):
+        self.disconnect_account(form.cleaned_data["provider"])
+        return super().form_valid(form)
