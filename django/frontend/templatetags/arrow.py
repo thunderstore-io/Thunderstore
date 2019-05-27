39858a79a66259060ba5193af16090c94ceff4dd
Mythic <mythicmaniac@gmail.com>
2019-05-27 05:02:10 +0300

    Improve timestamp formatting
    Format timestamps into a more human readable format (e.g. a month ago)
    by using arrow
    
    Resolves #88

---
+++ django/frontend/templatetags/arrow.py
@@ -0,0 +1,13 @@
+import arrow
+
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+
+@register.simple_tag
+def humanize_timestamp(timestamp):
+    timestamp = arrow.get(timestamp)
+    now = arrow.get(timezone.now())
+    return timestamp.humanize(now)
