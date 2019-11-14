ab539688b6f0ca4b272b915d187407f18eaf24e5
Mythic <mythicmaniac@gmail.com>
2019-11-14 20:45:33 +0200

    Add admin views for JWT auth models
    Add admin views for JWT authentication configurations

---
+++ django/core/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+
+from core.models import IncomingJWTAuthConfiguration
+
+
+@admin.register(IncomingJWTAuthConfiguration)
+class PackageAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "key_id",
+    )
+    list_display = (
+        "name",
+        "user",
+        "secret_type",
+        "key_id",
+    )
+    list_filter = (
+        "secret_type",
+    )
+    search_fields = (
+        "key_id",
+        "user__name",
+    )
