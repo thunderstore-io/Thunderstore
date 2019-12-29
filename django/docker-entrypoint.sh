bc7826d09d196057b42148a15453c631bb79bd68
Mythic <mythicmaniac@gmail.com>
2019-12-29 18:55:31 +0200

    Make gunicorn parameters configurable
    Make the gunicorn log level and worker count configurable via
    environment variables

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+workers=${GUNICORN_WORKER_COUNT:-2}
+loglevel=${GUNICORN_LOG_LEVEL:-info}
+
 if [ "$DEBUG" = true ] || [ "$DEBUG" = 'True' ] || [ "$DEBUG" = 1 ]; then
     echo 'Launching in debug mode'
     python manage.py runserver 0.0.0.0:8000
 else
     echo 'Launching in production mode'
     python manage.py migrate
-    gunicorn core.wsgi:application --log-level debug -w 2 -k gevent -b 0.0.0.0:8000
+    echo "gunicorn core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000"
+    gunicorn core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000
 fi
