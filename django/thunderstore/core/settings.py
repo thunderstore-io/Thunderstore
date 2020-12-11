7ef74be5085dd91456c6b60685acef563cad0ff3
Mythic <mythicmaniac@gmail.com>
2020-12-11 14:47:22 +0200

    Enable Django's built-in multi-site support
    Enable the built-in multi-site support of Django in preparation for
    enabling Thunderstore to operate under multiple different domains
    
    No refs

--- django/thunderstore/core/settings.py
+++ django/thunderstore/core/settings.py
@@ -138,6 +138,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django.contrib.sites',
 
     # 3rd Party
     'easy_thumbnails',
