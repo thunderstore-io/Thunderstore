a42dd659775a623590bb2ffa3bf8ece02a27b60a
Nihaal Sangha <nihaal.git@gmail.com>
2020-12-21 00:10:54 +0000

    Fix migration dependency

--- django/thunderstore/repository/migrations/0024_add_license.py
+++ django/thunderstore/repository/migrations/0024_add_license.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("repository", "0022_add_package_listing"),
+        ("repository", "0023_add_display_name"),
     ]
 
     operations = [
