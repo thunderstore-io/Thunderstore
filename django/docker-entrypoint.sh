0ee7b0bfbdc06f73ed10a9e07baa7c9b3813ecea
Mythic <mythicmaniac@gmail.com>
2021-01-13 12:07:48 +0200

    Add kubernetess health probe scripts
    Add scripts to be used with kubernetes liveness and readiness checks

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -4,6 +4,7 @@ workers=${GUNICORN_WORKER_COUNT:-2}
 loglevel=${GUNICORN_LOG_LEVEL:-info}
 maxrequests=${GUNICORN_MAX_REQUESTS:-10000}
 maxrequestsjitter=${GUNICORN_MAX_REQUESTS_JITTER:-1000}
+pidfile=${GUNICORN_PIDFILE:-'/var/run/gunicorn.pid'}
 
 export >> ~/.profile
 
@@ -13,6 +14,6 @@ if [ "$DEBUG" = true ] || [ "$DEBUG" = 'True' ] || [ "$DEBUG" = 1 ]; then
 else
     echo 'Launching in production mode'
     python manage.py migrate
-    echo "gunicorn thunderstore.core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000 --max-requests $maxrequests --max-requests-jitter $maxrequestsjitter"
-    gunicorn thunderstore.core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000
+    echo "gunicorn thunderstore.core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000 --max-requests $maxrequests --max-requests-jitter $maxrequestsjitter --pid $pidfile"
+    gunicorn thunderstore.core.wsgi:application --log-level $loglevel -w $workers -k gevent -b 0.0.0.0:8000 --pid $pidfile
 fi
