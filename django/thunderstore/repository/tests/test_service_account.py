e0c5a4da401425015b2625579b3a3ca909efd25f
Nihaal Sangha <nihaal.git@gmail.com>
2021-01-17 15:07:25 +0000

    Rename service account tests

--- django/thunderstore/repository/tests/test_service_account.py
+++ django/thunderstore/repository/tests/test_service_account.py
@@ -8,7 +8,7 @@ from thunderstore.repository.service_account import CreateServiceAccountForm
 
 
 @pytest.mark.django_db
-def test_create_service_account(user, uploader_identity):
+def test_service_account_create(user, uploader_identity):
     UploaderIdentityMember.objects.create(
         user=user,
         identity=uploader_identity,
@@ -21,7 +21,7 @@ def test_create_service_account(user, uploader_identity):
 
 
 @pytest.mark.django_db
-def test_create_service_account_not_member(user, uploader_identity):
+def test_service_account_create_not_member(user, uploader_identity):
     form = CreateServiceAccountForm(user, data={"identity": uploader_identity})
     assert form.is_valid() is False
     assert len(form.errors["identity"]) == 1
@@ -32,7 +32,7 @@ def test_create_service_account_not_member(user, uploader_identity):
 
 
 @pytest.mark.django_db
-def test_create_service_account_not_owner(user, uploader_identity):
+def test_service_account_create_not_owner(user, uploader_identity):
     UploaderIdentityMember.objects.create(
         user=user,
         identity=uploader_identity,
