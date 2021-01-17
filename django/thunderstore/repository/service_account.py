33ad27507ba9846fdebe3f4e313857a29baf6dab
Nihaal Sangha <nihaal.git@gmail.com>
2021-01-17 15:49:52 +0000

    Add delete service account form

--- django/thunderstore/repository/service_account.py
+++ django/thunderstore/repository/service_account.py
@@ -34,3 +34,24 @@ class CreateServiceAccountForm(forms.Form):
             is_service_account=True,
             owner=self.cleaned_data["identity"],
         )
+
+
+class DeleteServiceAccountForm(forms.Form):
+    def __init__(self, user: User, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.user = user
+        self.fields["service_account"] = forms.ModelChoiceField(
+            queryset=ServiceAccountMetadata.objects.filter(
+                owner__in=UploaderIdentity.objects.filter(members__user=user),
+            ),
+        )
+
+    def clean_service_account(self) -> ServiceAccountMetadata:
+        service_account = self.cleaned_data["service_account"]
+        member = service_account.owner.members.get(user=self.user)
+        if member.role != UploaderIdentityMemberRole.owner:
+            raise ValidationError("Must be identity owner to delete a service account")
+        return service_account
+
+    def save(self) -> None:
+        self.cleaned_data["service_account"].delete()
--- django/thunderstore/repository/tests/test_service_account.py
+++ django/thunderstore/repository/tests/test_service_account.py
@@ -1,10 +1,14 @@
 import pytest
 
+from thunderstore.core.factories import UserFactory
 from thunderstore.repository.models import (
     UploaderIdentityMember,
     UploaderIdentityMemberRole,
 )
-from thunderstore.repository.service_account import CreateServiceAccountForm
+from thunderstore.repository.service_account import (
+    CreateServiceAccountForm,
+    DeleteServiceAccountForm,
+)
 
 
 @pytest.mark.django_db
@@ -50,4 +54,52 @@ def test_service_account_create_not_owner(user, uploader_identity):
     assert (
         form.errors["identity"][0]
         == "Must be identity owner to create a service account"
+    )
+
+
+@pytest.mark.django_db
+def test_service_account_delete(service_account):
+    member = service_account.owner.members.first()
+    assert member.role == UploaderIdentityMemberRole.owner
+    form = DeleteServiceAccountForm(
+        member.user,
+        data={"service_account": service_account},
+    )
+    assert form.is_valid()
+    form.save()
+
+
+@pytest.mark.django_db
+def test_service_account_delete_not_member(service_account):
+    user = UserFactory.create()
+    assert service_account.owner.members.filter(user=user).exists() is False
+    form = DeleteServiceAccountForm(
+        user,
+        data={"service_account": service_account},
+    )
+    assert form.is_valid() is False
+    assert len(form.errors["service_account"]) == 1
+    assert (
+        form.errors["service_account"][0]
+        == "Select a valid choice. That choice is not one of the available choices."
+    )
+
+
+@pytest.mark.django_db
+def test_service_account_delete_not_owner(service_account):
+    user = UserFactory.create()
+    UploaderIdentityMember.objects.create(
+        user=user,
+        identity=service_account.owner,
+        role=UploaderIdentityMemberRole.member,
+    )
+    form = DeleteServiceAccountForm(
+        user,
+        data={"service_account": service_account},
+    )
+    assert form.is_valid() is False
+    assert len(form.errors["service_account"]) == 1
+    assert (
+        form.errors["service_account"][0]
+        == "Must be identity owner to delete a service account"
     )
