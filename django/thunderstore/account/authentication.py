5732cfcae42e2a9a1ffdf0e74f0bddf9cb28092c
Nihaal Sangha <nihaal.git@gmail.com>
2021-01-30 14:20:13 +0000

    Add last_used update

--- django/thunderstore/account/authentication.py
+++ django/thunderstore/account/authentication.py
@@ -1,5 +1,21 @@
+import datetime
+
 from rest_framework.authentication import TokenAuthentication as DRFTokenAuthentication
+from rest_framework.authtoken.models import Token
+
+from thunderstore.account.models import ServiceAccount
 
 
 class TokenAuthentication(DRFTokenAuthentication):
     keyword = "Bearer"
+
+    def authenticate(self, request):
+        out = super().authenticate(request)
+        if out is not None and all(out):
+            # The request has been authenticated
+            token: Token = out[1]
+            service_account = ServiceAccount.objects.filter(user=token.user).first()
+            if service_account:
+                service_account.last_used = datetime.datetime.now()
+                service_account.save()
+        return out
