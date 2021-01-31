f888f423394a3639e749b172b2e48919be7ee3e1
Nihaal Sangha <nihaal.git@gmail.com>
2021-01-31 18:19:03 +0000

    Remove unnecessary mark.django_db

--- django/thunderstore/account/test_service_account.py
+++ django/thunderstore/account/test_service_account.py
@@ -227,7 +227,6 @@ def test_service_account_create_token(service_account):
     assert service_account.user == token.user
 
 
-@pytest.mark.django_db
 def test_service_account_token_fixture(service_account_token):
     assert service_account_token.user.service_account
 
