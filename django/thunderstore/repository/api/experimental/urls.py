be7b258bdd8d709934cebbe6cd72fb775d1511a6
Mythic <mythicmaniac@gmail.com>
2021-02-11 19:37:04 +0200

    Add user info API view to experimental API

--- django/thunderstore/repository/api/experimental/urls.py
+++ django/thunderstore/repository/api/experimental/urls.py
@@ -4,8 +4,12 @@ from thunderstore.repository.api.experimental.views import (
     PackageListApiView,
     UploadPackageApiView,
 )
+from thunderstore.social.api.experimental.views import CurrentUserExperimentalApiView
 
 urls = [
+    path(
+        "current-user/", CurrentUserExperimentalApiView.as_view(), name="current-user"
+    ),
     path("package/", PackageListApiView.as_view(), name="package-list"),
     path("package/upload/", UploadPackageApiView.as_view(), name="package-upload"),
 ]
---
+++ django/thunderstore/social/api/experimental/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class CurrentUserExperimentalApiView(APIView):
+    """
+    Gets information about the current user, such as rated packages and permissions
+    """
+
+    def get(self, request, format=None):
+        capabilities = set()
+        rated_packages = []
+        if request.user.is_authenticated:
+            capabilities.add("package.rate")
+            rated_packages = request.user.package_ratings.select_related(
+                "package"
+            ).values_list("package__uuid4", flat=True)
+        return Response({"capabilities": capabilities, "ratedPackages": rated_packages})
