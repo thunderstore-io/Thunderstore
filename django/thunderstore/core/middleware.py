6d2115eaa94e85f6e906cfc2c8bb608b3a0bc91c
Mythic <mythicmaniac@gmail.com>
2021-03-07 12:51:33 +0200

    Add middleware for tracking query count
    Add a middleware which will output the amount of queries executed when
    handling a request as a HTTP response header called 'Django-Query-Count'

---
+++ django/thunderstore/core/middleware.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+
+
+class QueryCountHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not settings.DEBUG:
+            return self.get_response(request)
+        else:
+            from django.db import connection
+            from django.test.utils import CaptureQueriesContext
+
+            with CaptureQueriesContext(connection) as context:
+                response = self.get_response(request)
+            response["Django-Query-Count"] = len(context)
+            print("=" * 20)
+            print(f"Executed {len(context)} queries")
+            return response
