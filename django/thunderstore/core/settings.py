5acf0285f08efddb7e38c8966e910c564e2e443e
Mythic <mythicmaniac@gmail.com>
2021-03-08 05:25:35 +0200

    Change local server to run through gunicorn
    As there doesn't really seem to be any downsides to using gunicorn
    locally as well, swap to it in an effort to replicate the production
    environment more closely

--- django/thunderstore/core/settings.py
+++ django/thunderstore/core/settings.py
@@ -307,27 +307,27 @@ if REDIS_URL:
 #         }
 #     }
 
-LOGGING = {
-    "version": 1,
-    "filters": {
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        }
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "filters": ["require_debug_true"],
-            "class": "logging.StreamHandler",
-        }
-    },
-    "loggers": {
-        "django.db.backends": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-        }
-    },
-}
+# LOGGING = {
+#     "version": 1,
+#     "filters": {
+#         "require_debug_true": {
+#             "()": "django.utils.log.RequireDebugTrue",
+#         }
+#     },
+#     "handlers": {
+#         "console": {
+#             "level": "DEBUG",
+#             "filters": ["require_debug_true"],
+#             "class": "logging.StreamHandler",
+#         }
+#     },
+#     "loggers": {
+#         "django.db.backends": {
+#             "level": "DEBUG",
+#             "handlers": ["console"],
+#         }
+#     },
+# }
 
 # REST Framework
 
