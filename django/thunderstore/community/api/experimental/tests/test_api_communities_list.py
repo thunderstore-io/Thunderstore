f4596bafc4a5865fd2054b2ebfd0af04fb5bf16f
Nihaal Sangha <nihaal.git@gmail.com>
2021-03-20 12:56:27 +0000

    Add community API tests

---
+++ django/thunderstore/community/api/experimental/tests/test_api_communities_list.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_api_experimental_communities_list(
+    api_client,
+):
+    response = api_client.get(
+        "/api/experimental/community/",
+        HTTP_ACCEPT="application/json",
+    )
+    assert response.status_code == 200
+    assert isinstance(response.json()["communities"], list)
