e720a651c43f93dbe8178bebd4af2b17bd4a1912
Nihaal Sangha <nihaal.git@gmail.com>
2021-03-20 18:05:00 +0000

    Fix parent related name conflict

--- django/thunderstore/repository/models/thread.py
+++ django/thunderstore/repository/models/thread.py
@@ -10,6 +10,22 @@ class Thread(models.Model):
         primary_key=True,
     )
 
+    @property
+    def parent(self):
+        # TODO: Use SupportedParentTypes
+        for supported_parent_type_name, supported_parent_type_app_name in (
+            ("packagelisting", "community"),
+            ("comment", "repository"),
+        ):
+            parent = getattr(
+                self,
+                f"parent_{supported_parent_type_app_name}_{supported_parent_type_name}",
+                None,
+            )
+            if parent:
+                return parent
+        raise Exception("Parent not found")
+
 
 class CommentsThreadMixin(models.Model):
     """Mixin used to add comments to a model."""
@@ -17,7 +33,7 @@ class CommentsThreadMixin(models.Model):
     comments_thread = models.OneToOneField(
         Thread,
         on_delete=models.SET_NULL,
-        related_name="parent",
+        related_name="parent_%(app_label)s_%(class)s",
         null=True,
     )
 
