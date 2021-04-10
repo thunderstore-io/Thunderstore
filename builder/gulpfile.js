02498b1a8dde0ff73ca9e4465c25ea4b30adfcfd
Mythic <mythicmaniac@gmail.com>
2021-04-10 18:26:37 +0300

    Add TypeScript build support
    Add TypeScript build support to the gulp build setup. Converting the
    current setup to webpack was also considered, but would have been more
    effort to get everything playing nicely.

--- builder/gulpfile.js
+++ builder/gulpfile.js
@@ -4,10 +4,14 @@ const clean_css = require("gulp-clean-css");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const order = require("gulp-order");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const tsify = require("tsify");
+const merge = require("merge-stream");
 
-function js() {
-    return src([
-        "src/js/**/*.js",
+function js(debug) {
+    const vendorBuild = src([
         "node_modules/jquery/dist/jquery.js",
         "node_modules/popper.js/dist/umd/popper.js",
         "node_modules/bootstrap/dist/js/bootstrap.js",
@@ -20,18 +24,48 @@ function js() {
                     "node_modules/popper.js/dist/umd/popper.js",
                     "node_modules/bootstrap/dist/js/bootstrap.js",
                     "node_modules/slim-select/dist/slimselect.js",
-                    "src/js/**/*.js",
                 ],
                 { base: "./" }
             )
         )
-        .pipe(concat("all.js"))
-        .pipe(uglify())
-        .pipe(dest("./build/js/"));
+        .pipe(concat("vendor.js"));
+
+    const tsBuild = browserify({
+        basedir: ".",
+        debug: debug,
+        entries: ["src/main.ts"],
+        cache: {},
+        packageCache: {},
+    })
+        .plugin(tsify)
+        .bundle()
+        .on("error", (err) => console.log(err))
+        .pipe(source("bundle.js"));
+
+    let result = merge(vendorBuild, tsBuild)
+        .pipe(buffer())
+        .pipe(order(["vendor.js", "bundle.js"]))
+        .pipe(concat("all.js"));
+    if (!debug) {
+        result = result.pipe(uglify());
+    }
+    return result.pipe(dest("./build/js/"));
+}
+
+function js_prod() {
+    return js(false);
+}
+
+function js_debug() {
+    return js(true);
 }
 
 function watch_js() {
-    watch("./src/js/**/*.js", { usePolling: true }, js);
+    return watch(
+        ["./src/**/*.ts", "./src/**/*.tsx", "./src/**/*.js"],
+        { usePolling: true },
+        js_debug
+    );
 }
 
 function css() {
@@ -49,5 +83,5 @@ function watch_css() {
     );
 }
 
-exports.watch = parallel(js, watch_js, css, watch_css);
-exports.default = parallel(js, css);
+exports.watch = parallel(js_debug, watch_js, css, watch_css);
+exports.default = parallel(js_prod, css);
