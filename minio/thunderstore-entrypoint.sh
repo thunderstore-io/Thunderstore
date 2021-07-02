e869a6446f311109d26b2504a3016cdf4ac345c5
Mythic <mythicmaniac@gmail.com>
2021-07-02 19:37:25 +0300

    Upgrade minio
    Upgrade minio and include tag name in the version pin

--- minio/thunderstore-entrypoint.sh
+++ minio/thunderstore-entrypoint.sh
@@ -2,7 +2,8 @@
 sh /usr/bin/docker-entrypoint.sh "$@" &
 MINIO_PID=$!
 while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done;
-mc config host add thunderstore http://127.0.0.1:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD;
+sleep 2
+mc alias set thunderstore http://127.0.0.1:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD;
 mc mb thunderstore/thunderstore;
 mc policy set download thunderstore/thunderstore;
 kill $MINIO_PID
