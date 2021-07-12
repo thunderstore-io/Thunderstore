f9350f9df98f7afa9e9ac8be6120123a42f7abd7
Nathan Gill <nrgill28@gmail.com>
2021-07-12 16:32:44 -0400

    Use correct user model and display social connections

--- django/thunderstore/account/views.py
+++ django/thunderstore/account/views.py
@@ -1,8 +1,8 @@
-from thunderstore.repository.models.package import Package
-from thunderstore.repository.models.uploader_identity import UploaderIdentity
 from django.shortcuts import get_object_or_404
+from thunderstore.repository.models.package import Package
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 
 
 class ProfileView(TemplateView):
@@ -10,11 +10,17 @@ class ProfileView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
+        # Get the user
         username = kwargs["user"]
-        context['user'] = get_object_or_404(UploaderIdentity, name=username)
+        context['user'] = get_object_or_404(User, username=username)
+        context['social_providers'] = context['user'].social_auth.all()
+
+        # Get the packages from this user and total up the stats
         userpackages = sorted(Package.objects.filter(owner__name=username).all(), key=lambda x: x.downloads)
         context['popular_packages'] = userpackages[:4]
         context['package_count'] = len(userpackages)
         context['total_downloads'] = sum(package.downloads for package in userpackages)
         context['total_likes'] = sum(package.rating_score for package in userpackages)
+
         return context
