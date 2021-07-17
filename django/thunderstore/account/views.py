f5fa444fde32a2cfcbc7ca06d61d9515ffa6995e
Nathan Gill <nrgill28@gmail.com>
2021-07-16 22:30:53 -0400

    Use Gravatar for avatars

--- django/thunderstore/account/views.py
+++ django/thunderstore/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from thunderstore.repository.models.package import Package
 from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User
+import hashlib
 
 
 
@@ -22,5 +23,6 @@ class ProfileView(TemplateView):
         context['package_count'] = len(userpackages)
         context['total_downloads'] = sum(package.downloads for package in userpackages)
         context['total_likes'] = sum(package.rating_score for package in userpackages)
+        context['email_hash'] = hashlib.md5(context['user'].email.lower().encode()).hexdigest()
 
         return context
