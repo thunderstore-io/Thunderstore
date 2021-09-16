ba4e946475368f3049b555f9ad74e342563a4ca2
Oksamies <asko.soisalo@gmail.com>
2021-09-16 15:36:04 +0300

    Add a test for add_auth_login_link
    refs #1dneg95

--- django/thunderstore/frontend/tests/test_views.py
+++ django/thunderstore/frontend/tests/test_views.py
@@ -18,3 +18,19 @@ def test_views_manifest_v1_validator(client, community_site):
         HTTP_HOST=community_site.site.domain,
     )
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_views_auth_login_link_generation(client, community_site):
+    response = client.get(
+        reverse("packages.list"),
+        HTTP_HOST=community_site.site.domain,
+    )
+    assert (
+        b"/auth/login/github/?next=http%3A%2F%2Ftestsite.test%2Fpackage%2F"
+        in response._container[0]
+    )
+    assert (
+        b"/auth/login/discord/?next=http%3A%2F%2Ftestsite.test%2Fpackage%2F"
+        in response._container[0]
+    )
