d57124c4d2a773052f11abef818ae467dc350d48
Mythic <mythicmaniac@gmail.com>
2021-09-20 03:04:52 +0300

    Add support for exclusive auth domain
    Add AUTH_EXCLUSIVE_HOST and SOCIAL_AUTH_INIT_HOST which can be used to
    control the auth process in more detail.
    
    SOCIAL_AUTH_INIT_HOST will be used as the path to initialize new auth
    sessions from, and as such will also receive the auth callbacks.
    
    AUTH_EXCLUSIVE_HOST will be used as a default for SOCIAL_AUTH_INIT_HOST
    if set, and the domain set to AUTH_EXCLUSIVE_HOST will only support auth
    process related views.

--- django/thunderstore/frontend/templatetags/auth_login.py
+++ django/thunderstore/frontend/templatetags/auth_login.py
@@ -1,24 +1,36 @@
 from urllib.parse import ParseResult, parse_qs, urlencode, urlparse, urlunparse
 
 from django import template
+from django.conf import settings
 from django.urls import reverse
 from django.utils.http import urlencode
 
 register = template.Library()
 
 
+def get_auth_init_host(request_host: str) -> str:
+    return (
+        settings.SOCIAL_AUTH_INIT_HOST or settings.AUTH_EXCLUSIVE_HOST or request_host
+    )
+
+
 @register.simple_tag(takes_context=True)
-def add_auth_login_link(context, backend):
-    parts = urlparse(reverse("social:begin", kwargs={"backend": backend}))
+def add_auth_login_link(context, backend) -> str:
+    parts = urlparse(
+        context.request.build_absolute_uri(
+            reverse("social:begin", kwargs={"backend": backend})
+        )
+    )
     query_dict = parse_qs(parts.query)
-    query_dict["next"] = [context.request.build_absolute_uri()]
+    query_dict[b"next"] = [context.request.build_absolute_uri()]
+    netloc = get_auth_init_host(parts.netloc)
+    scheme = (parts.scheme or "https") if netloc else ""
     modified = ParseResult(
-        scheme=parts.scheme,
-        netloc=parts.netloc,
+        scheme=scheme,
+        netloc=netloc,
         path=parts.path,
         params=parts.params,
         query=urlencode(query_dict, doseq=True),
         fragment=parts.fragment,
     )
-    result = urlunparse(modified)
-    return result
+    return str(urlunparse(modified))
--- django/thunderstore/frontend/tests/test_views.py
+++ django/thunderstore/frontend/tests/test_views.py
@@ -21,16 +21,54 @@ def test_views_manifest_v1_validator(client, community_site):
 
 
 @pytest.mark.django_db
-def test_views_auth_login_link_generation(client, community_site):
+@pytest.mark.parametrize("auth_init_host", (None, "init.localhost"))
+@pytest.mark.parametrize("auth_exclusive_host", (None, "auth.localhost"))
+@pytest.mark.parametrize("secure", (False, True))
+def test_views_auth_login_link_generation(
+    client,
+    community_site,
+    settings,
+    auth_init_host: str,
+    auth_exclusive_host: str,
+    secure: bool,
+) -> None:
+    settings.SOCIAL_AUTH_INIT_HOST = auth_init_host
+    settings.AUTH_EXCLUSIVE_HOST = auth_exclusive_host
+    prefix = f"http{'s' if secure else ''}://{(auth_init_host or auth_exclusive_host) or community_site.site.domain}"
+    response = client.get(
+        reverse("packages.list"),
+        HTTP_HOST=community_site.site.domain,
+        secure=secure,
+    )
+    assert (
+        f"{prefix}/auth/login/github/?next=http{'s' if secure else ''}%3A%2F%2Ftestsite.test%2Fpackage%2F".encode()
+        in response.content
+    )
+    assert (
+        f"{prefix}/auth/login/discord/?next=http{'s' if secure else ''}%3A%2F%2Ftestsite.test%2Fpackage%2F".encode()
+        in response.content
+    )
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("backend", ("discord", "github"))
+def test_views_disabled_for_auth_exclusive_host(
+    client, community_site, settings, backend: str
+):
     response = client.get(
         reverse("packages.list"),
         HTTP_HOST=community_site.site.domain,
     )
-    assert (
-        b"/auth/login/github/?next=http%3A%2F%2Ftestsite.test%2Fpackage%2F"
-        in response._container[0]
+    assert response.status_code == 200
+    settings.AUTH_EXCLUSIVE_HOST = community_site.site.domain
+    response = client.get(
+        reverse("packages.list"),
+        HTTP_HOST=community_site.site.domain,
     )
-    assert (
-        b"/auth/login/discord/?next=http%3A%2F%2Ftestsite.test%2Fpackage%2F"
-        in response._container[0]
+    assert response.status_code == 404
+    assert response.content == b"Community not found"
+    response = client.get(
+        reverse("social:begin", kwargs={"backend": backend}),
+        HTTP_HOST=community_site.site.domain,
     )
+    assert response.status_code == 302
