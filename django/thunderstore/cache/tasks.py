b99b20efaeab87e37f67d6e24bc4419daf8cb724
Mythic <mythicmaniac@gmail.com>
2022-03-27 17:21:36 +0300

    Allow disabling of cache busting via settings

--- django/thunderstore/cache/tasks.py
+++ django/thunderstore/cache/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.conf import settings
 from django.core.cache import cache
 
 from thunderstore.cache.cache import CacheBustCondition
@@ -7,6 +8,8 @@ from thunderstore.utils.decorators import run_after_commit
 
 @run_after_commit
 def invalidate_cache_on_commit_async(cache_bust_condition: str):
+    if cache_bust_condition in settings.DISABLED_CACHE_BUST_CONDITIONS:
+        return
     invalidate_cache.delay(cache_bust_condition)
 
 
