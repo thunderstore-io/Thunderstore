97e7b4d1ab7292baa901f9569da90d49df6c7ac1
Oksamies <asko.soisalo@gmail.com>
2022-04-19 17:27:20 +0300

    Improve CommunityMixin

--- django/thunderstore/repository/mixins.py
+++ django/thunderstore/repository/mixins.py
@@ -1,3 +1,6 @@
+from thunderstore.community.models.community import Community
+
+
 class CommunityMixin:
     @property
     def community_identifier(self):
@@ -5,3 +8,12 @@ class CommunityMixin:
             self.kwargs.get("community_identifier", None)
             or self.request.community.identifier
         )
+
+    @property
+    def solved_community_from_identifier(self):
+        return Community.objects.get(identifier=self.community_identifier)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["community_identifier"] = self.community_identifier
+        return context
