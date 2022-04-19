579547aff13ee55f7ccb748d6ec6e8393e26181f
Oksamies <asko.soisalo@gmail.com>
2022-04-19 19:01:14 +0300

    Remove get install_url from templatetags
    Since it's now under the new just added templatetags

--- django/thunderstore/repository/templatetags/get_install_url.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def get_install_url(context, package_version):
-    return package_version.get_install_url(context["request"])
