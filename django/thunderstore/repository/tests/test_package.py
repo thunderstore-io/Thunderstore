63b9f655ff1fd934fd4bf11e9264019fb7c364c7
Mythic <mythicmaniac@gmail.com>
2022-05-04 05:04:57 +0300

    Use primary host fallback for URL building
    Use the PRIMARY_HOST setting as a fallback when building a package's
    full URL if a Site isn't provided.
    
    Refs TS-383

--- django/thunderstore/repository/tests/test_package.py
+++ django/thunderstore/repository/tests/test_package.py
@@ -1,6 +1,14 @@
+from typing import Any
+
 import pytest
 
+from thunderstore.community.factories import (
+    CommunityFactory,
+    CommunitySiteFactory,
+    SiteFactory,
+)
 from thunderstore.community.models.package_listing import PackageListing
+from thunderstore.repository.models import Package
 
 
 @pytest.mark.django_db
@@ -30,3 +38,24 @@ def test_package_get_page_url(
         active_package_listing.community.identifier
     )
     assert owner_url == f"/c/test/p/Test_Team/{active_package_listing.package.name}/"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "site_host", ("thunderstore.dev", "test.thunderstore.dev", None)
+)
+@pytest.mark.parametrize(
+    "primary_host", ("thunderstore.io", "stonderthure.io.example.org")
+)
+def test_package_get_full_url(
+    settings: Any,
+    site_host: str,
+    primary_host: str,
+    active_package: Package,
+) -> None:
+    site = SiteFactory(domain=site_host) if site_host is not None else None
+    settings.PRIMARY_HOST = primary_host
+
+    expected_host = site_host if site else primary_host
+    expected_url = f"{settings.PROTOCOL}{expected_host}/package/{active_package.namespace.name}/{active_package.name}/"
+    assert active_package.get_full_url(site=site) == expected_url
