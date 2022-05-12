425e4c6edaa08ea77447627c7db9a0cabee03f48
Oksamies <asko.soisalo@gmail.com>
2022-05-12 22:28:46 +0300

    Turn error page views to class based
    e.g. Handle404 and Handle500
    This has to be done, for the base template to work with those views
    Also fixup the tests
    
    refs TS-272

--- django/thunderstore/frontend/tests/test_errors.py
+++ django/thunderstore/frontend/tests/test_errors.py
@@ -4,20 +4,30 @@ import pytest
 from django.conf import settings
 from django.test import RequestFactory
 
-from thunderstore.frontend.views import handle404, handle500
+from thunderstore.community.models.community import Community
+from thunderstore.community.models.community_site import CommunitySite
+from thunderstore.frontend.views import Handler404, Handler500
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("static", (False, True))
-@pytest.mark.parametrize("view", (handle404, handle500))
-def test_frontend_error_cache_headers(rf: RequestFactory, static: bool, view):
+@pytest.mark.parametrize("view", (Handler404.as_view, Handler500.as_view))
+def test_frontend_error_cache_headers(
+    community: Community,
+    community_site: CommunitySite,
+    rf: RequestFactory,
+    static: bool,
+    view,
+):
     # b64 to ensure no accidental overlap with setting value if not intended
     path = (
         settings.STATIC_URL
         if static
         else base64.b64encode(settings.STATIC_URL.encode()).decode()
     )
-    response = view(rf.get(path))
+    request = rf.get(path)
+    request.community = community
+    response = view()(request)
     if static:
         assert response["Cache-Control"] == "no-cache"
     else:
