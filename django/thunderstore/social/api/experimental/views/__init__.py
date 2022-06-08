53d56a31d94b05acb2bf824c68f8ca3148ac7331
Antti Mäki <antti@anttimaki.com>
2022-06-08 08:03:06 +0300

    Experimental API: add endpoint for checking session validity
    Simple endpoint for checking if the user is authenticated, i.e. if the
    session key sent in the authorization header is related to active
    session stored in the database.
    
    The new UI will use this to check that the session key stored in the
    localStorage is still current.
    
    Refs TS-230, TS-358

--- django/thunderstore/social/api/experimental/views/__init__.py
+++ django/thunderstore/social/api/experimental/views/__init__.py
@@ -1,7 +1,9 @@
 from .complete_login import CompleteLoginApiView
 from .current_user import CurrentUserExperimentalApiView
+from .validate_session import ValidateSessionApiView
 
 __all__ = [
     "CompleteLoginApiView",
     "CurrentUserExperimentalApiView",
+    "ValidateSessionApiView",
 ]
---
+++ django/thunderstore/social/api/experimental/views/validate_session.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from drf_yasg.utils import swagger_auto_schema  # type: ignore
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_401_UNAUTHORIZED
+from rest_framework.views import APIView
+
+
+class ValidateSessionApiView(APIView):
+    """
+    Check that valid session key is provided in Authorization header.
+    """
+
+    @swagger_auto_schema(
+        operation_id="experimental.auth.validate",
+        responses={
+            200: "Session is valid",
+            401: "Session key is missing, invalid, or expired",
+        },
+    )
+    def get(self, request: Request) -> HttpResponse:
+        # UserSessionTokenAuthentication will automatically return 401
+        # if the given session key is not valid, but will do nothing if
+        # the session key isn't provided at all.
+        if request.user.is_authenticated:
+            return Response({"detail": "OK"})
+
+        return Response({"detail": "Invalid token."}, HTTP_401_UNAUTHORIZED)
