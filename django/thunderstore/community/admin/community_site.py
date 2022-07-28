6fbaf94fb7451a25fbd299388378e0d5e5db6447
Mythic <mythicmaniac@gmail.com>
2022-07-28 19:33:18 +0300

    Remove unused CommunitySite fields (#649)
    Now that CommunitySite metadata has been moved to the Community model,
    we can safely remove the fields from the CommunitySite model

--- django/thunderstore/community/admin/community_site.py
+++ django/thunderstore/community/admin/community_site.py
@@ -1,32 +1,16 @@
-from django import forms
 from django.contrib import admin
-from django.forms import PasswordInput, TextInput
 
 from ..models.community_site import CommunitySite
 
 
-class CommunitySiteAdminForm(forms.ModelForm):
-    class Meta:
-        model = CommunitySite
-        widgets = {
-            "social_auth_discord_key": TextInput(attrs={"size": 40}),
-            "social_auth_discord_secret": PasswordInput(
-                render_value=True, attrs={"size": 40}
-            ),
-            "social_auth_github_key": TextInput(attrs={"size": 40}),
-            "social_auth_github_secret": PasswordInput(
-                render_value=True, attrs={"size": 40}
-            ),
-        }
-        fields = "__all__"
-
-
 @admin.register(CommunitySite)
 class CommunitySiteAdmin(admin.ModelAdmin):
-    form = CommunitySiteAdminForm
-
     filter_horizontal = ()
     list_filter = ("is_listed",)
+    raw_id_fields = (
+        "site",
+        "community",
+    )
     list_display = (
         "id",
         "community",
@@ -49,8 +33,4 @@ class CommunitySiteAdmin(admin.ModelAdmin):
     readonly_fields = (
         "datetime_created",
         "datetime_updated",
-        "icon_width",
-        "icon_height",
-        "background_image_width",
-        "background_image_height",
     )
