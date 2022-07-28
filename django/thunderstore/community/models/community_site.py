e16599c7474a1421cba27a36a0a2f79cc46240dc
Mythic <mythicmaniac@gmail.com>
2022-07-28 19:14:50 +0300

    Remove unused CommunitySite fields
    Now that CommunitySite metadata has been moved to the Community model,
    we can safely remove the fields from the CommunitySite model

--- django/thunderstore/community/models/community_site.py
+++ django/thunderstore/community/models/community_site.py
@@ -32,69 +32,8 @@ class CommunitySite(TimestampMixin, models.Model):
     )
     is_listed = models.BooleanField(default=True)
 
-    slogan = models.CharField(max_length=512, blank=True, null=True)
-    description = models.CharField(max_length=512, blank=True, null=True)
-
-    icon = models.ImageField(
-        upload_to=get_community_filepath,
-        width_field="icon_width",
-        height_field="icon_height",
-        blank=True,
-        null=True,
-    )
-    icon_width = models.PositiveIntegerField(default=0)
-    icon_height = models.PositiveIntegerField(default=0)
-
-    background_image = models.ImageField(
-        upload_to=get_community_filepath,
-        width_field="background_image_width",
-        height_field="background_image_height",
-        blank=True,
-        null=True,
-    )
-    background_image_width = models.PositiveIntegerField(default=0)
-    background_image_height = models.PositiveIntegerField(default=0)
-
-    favicon = models.FileField(
-        upload_to=get_community_filepath,
-        validators=[FileExtensionValidator(allowed_extensions=["ico"])],
-        blank=True,
-        null=True,
-    )
-
-    social_auth_github_key = models.TextField(blank=True, null=True)
-    social_auth_github_secret = models.TextField(blank=True, null=True)
-
-    social_auth_discord_key = models.TextField(blank=True, null=True)
-    social_auth_discord_secret = models.TextField(blank=True, null=True)
-
     def __str__(self):
         return str(self.community)
-
-    def save(self, *args, **kwargs):
-        if not self.icon:
-            self.icon_width = 0
-            self.icon_height = 0
-            if "update_fields" in kwargs:
-                kwargs["update_fields"] = set(
-                    kwargs["update_fields"]
-                    + (
-                        "icon_width",
-                        "icon_height",
-                    )
-                )
-        if not self.background_image:
-            self.background_image_width = 0
-            self.background_image_height = 0
-            if "update_fields" in kwargs:
-                kwargs["update_fields"] = set(
-                    kwargs["update_fields"]
-                    + (
-                        "background_image_width",
-                        "background_image_height",
-                    )
-                )
-        return super().save(*args, **kwargs)
 
     @property
     def full_url(self) -> str:
