dad48ce29ef1fc9a35e297a5feeacf55922bc028
Mythic <mythicmaniac@gmail.com>
2022-12-19 23:04:05 +0200

    Split tasks across multiple task queues
    Split background tasks across multiple celery queues. In particular,
    create a dedicated task queue for the generally slower cache update
    tasks.
    
    This allows separate background workers to be deployed for each queue,
    which should in turn make it easier to efficiently distribute server
    resources.
    
    Refs TS-1381

--- django/thunderstore/repository/tasks.py
+++ django/thunderstore/repository/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task
 
+from thunderstore.core.settings import CeleryQueues
 from thunderstore.repository.api.v1.tasks import update_api_v1_caches
 
 
-@shared_task
+@shared_task(queue=CeleryQueues.BackgroundCache)
 def update_api_caches():
     update_api_v1_caches()
--- django/thunderstore/usermedia/tasks.py
+++ django/thunderstore/usermedia/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task
 
+from thunderstore.core.settings import CeleryQueues
 from thunderstore.usermedia.cleanup import cleanup_expired_uploads
 
 
-@shared_task
+@shared_task(queue=CeleryQueues.Default)
 def celery_cleanup_expired_uploads():
     cleanup_expired_uploads()
