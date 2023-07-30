1a4c392eb5366f3a92e44760e722442320fca7ec
Mythic <mythicmaniac@gmail.com>
2023-07-30 12:50:40 +0300

    Take SchemaServerAppConfig into use
    Add a missing __init__.py configuration that enables the
    SchemaServerAppConfig for the thunderstore.schema_server module.

---
+++ django/thunderstore/schema_server/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "thunderstore.schema_server.apps.SchemaServerAppConfig"
--- django/thunderstore/schema_server/apps.py
+++ django/thunderstore/schema_server/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class SchemaServerAppConfig(AppConfig):
     name = "thunderstore.schema_server"
     label = "schema_server"
-    verbose_name = "Schema Server"
+    verbose_name = "schema server"
