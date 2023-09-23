c9c991c646fb7f6eb48d885a2ca8ac4fcda555de
Mythic <mythicmaniac@gmail.com>
2023-09-23 22:36:29 +0300

    Move thunderstore.repository tasks.py into a dir
    Convert the thunderstore.repository.tasks python module from a single
    file into a directory

---
+++ django/thunderstore/repository/tasks/__init__.py
@@ -0,0 +1,1 @@
+from .caches import *
--- django/thunderstore/repository/tasks/caches.py
+++ django/thunderstore/repository/tasks/caches.py
@@ -4,6 +4,9 @@ from thunderstore.core.settings import CeleryQueues
 from thunderstore.repository.api.v1.tasks import update_api_v1_caches
 
 
-@shared_task(queue=CeleryQueues.BackgroundCache)
+@shared_task(
+    name="thunderstore.repository.tasks.update_api_caches",
+    queue=CeleryQueues.BackgroundCache,
+)
 def update_api_caches():
     update_api_v1_caches()
