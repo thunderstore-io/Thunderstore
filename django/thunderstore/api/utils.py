9ab27862433713d45ed4027a5e6edebaee5afe31
Antti Mäki <antti@anttimaki.com>
2023-10-11 16:08:05 +0300

    Add CyberstormAutoSchemaMixin
    Alternative way to use conditional_swagger_auto_schema in DRF views.

--- django/thunderstore/api/utils.py
+++ django/thunderstore/api/utils.py
@@ -9,3 +9,16 @@ def conditional_swagger_auto_schema(*args, **kwargs):
         return swagger_auto_schema(*args, auto_schema=None, **kwargs)(f)
 
     return decorator
+
+
+class CyberstormAutoSchemaMixin:  # pragma: no cover
+    """
+    Control Cyberstorm API endpoint visibility in Swagger docs.
+
+    Use SHOW_CYBERSTORM_API_DOCS env variable to control whether the
+    endpoints are included in the API documentation or not.
+    """
+
+    @conditional_swagger_auto_schema(tags=["cyberstorm"])
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
