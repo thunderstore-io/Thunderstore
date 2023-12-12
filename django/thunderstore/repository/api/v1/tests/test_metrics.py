f6c5dd3964958680e2985c8c9052c8d48ad3a74a
Mythic <mythicmaniac@gmail.com>
2023-12-12 17:54:37 +0200

    Add package metrics endpoints
    Add endpoints for fetching metrics related to Package and
    PackageVersion objects.
    
    These endpoints are added under the v1 API as they should be kept
    stable.
    
    Resolves #948

---
+++ django/thunderstore/repository/api/v1/tests/test_metrics.py
@@ -0,0 +1,49 @@
+import pytest
+from rest_framework.test import APIClient
+
+from thunderstore.repository.models import Package, PackageRating
+
+
+@pytest.mark.django_db
+def test_api_v1_package_metrics(
+    user,
+    api_client: APIClient,
+    active_package: Package,
+) -> None:
+    active_package.latest.downloads = 200
+    active_package.latest.save()
+
+    PackageRating.objects.get_or_create(rater=user, package=active_package)
+
+    assert active_package.rating_score == 1
+    assert active_package.downloads == 200
+
+    namespace = active_package.namespace.name
+    name = active_package.name
+    response = api_client.get(f"/api/v1/package-metrics/{namespace}/{name}/")
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result["downloads"] == 200
+    assert result["rating_score"] == 1
+
+
+@pytest.mark.django_db
+def test_api_v1_package_version_metrics(
+    user,
+    api_client: APIClient,
+    active_package: Package,
+) -> None:
+    active_package.latest.downloads = 200
+    active_package.latest.save()
+
+    assert active_package.downloads == 200
+
+    namespace = active_package.namespace.name
+    name = active_package.name
+    version = active_package.latest.version_number
+    response = api_client.get(f"/api/v1/package-metrics/{namespace}/{name}/{version}/")
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result["downloads"] == 200
---
+++ django/thunderstore/repository/api/v1/views/metrics.py
@@ -0,0 +1,54 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from thunderstore.repository.models import Package, PackageVersion
+
+
+class PackageMetricsSerializer(serializers.Serializer):
+    downloads = serializers.IntegerField()
+    rating_score = serializers.IntegerField()
+
+
+# This view is potentialy used by the shields.io project and should be kept
+# backwards-compatible
+class PackageMetricsApiView(APIView):
+    @swagger_auto_schema(
+        operation_id="api_v1_package_metrics",
+        responses={200: PackageMetricsSerializer()},
+        tags=["v1"],
+    )
+    def get(self, request, namespace: str, name: str):
+        obj = get_object_or_404(
+            Package.objects.active(),
+            is_active=True,
+            namespace__name=namespace,
+            name=name,
+        )
+        return Response(PackageMetricsSerializer(obj).data)
+
+
+class PackageVersionMetricsSerializer(serializers.Serializer):
+    downloads = serializers.IntegerField()
+
+
+# This view is potentialy used by the shields.io project and should be kept
+# # backwards-compatible
+class PackageVersionMetricsApiView(APIView):
+    @swagger_auto_schema(
+        operation_id="api_v1_package_version_metrics",
+        responses={200: PackageVersionMetricsSerializer()},
+        tags=["v1"],
+    )
+    def get(self, request, namespace: str, name: str, version: str):
+        obj = get_object_or_404(
+            PackageVersion.objects.active(),
+            is_active=True,
+            package__is_active=True,
+            package__namespace__name=namespace,
+            name=name,
+            version_number=version,
+        )
+        return Response(PackageVersionMetricsSerializer(obj).data)
