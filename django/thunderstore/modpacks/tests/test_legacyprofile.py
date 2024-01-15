86d329a8481a877a4d688b2a19d7284d511d5671
Mythic <mythicmaniac@gmail.com>
2024-01-15 05:24:40 +0200

    Add caching to profile storage size check
    Add a counter based cache with a 10 minute refresh time to the total
    profile storage size check.

--- django/thunderstore/modpacks/tests/test_legacyprofile.py
+++ django/thunderstore/modpacks/tests/test_legacyprofile.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.cache import cache
 
 from thunderstore.modpacks.factories import LegacyProfileFactory
 from thunderstore.modpacks.models import LegacyProfile
@@ -8,11 +9,15 @@ from thunderstore.modpacks.models import LegacyProfile
 def test_legacyprofile_manager_get_total_used_disk_space():
     assert LegacyProfile.objects.count() == 0
     assert LegacyProfile.objects.get_total_used_disk_space() == 0
+
     p1 = LegacyProfileFactory()
+    cache.delete(LegacyProfile.objects.size_cache())
     assert LegacyProfile.objects.count() == 1
     assert LegacyProfile.objects.get_total_used_disk_space() == p1.file_size
+
     p2 = LegacyProfileFactory(file_size=32)
     p3 = LegacyProfileFactory(file_size=32890)
+    cache.delete(LegacyProfile.objects.size_cache())
     assert LegacyProfile.objects.count() == 3
     assert LegacyProfile.objects.get_total_used_disk_space() == (
         p1.file_size + p2.file_size + p3.file_size
