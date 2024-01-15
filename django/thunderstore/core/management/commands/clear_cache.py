c4806c1104ff054cd0fc7510dc2b2c0de33b9b1a
Mythic <mythicmaniac@gmail.com>
2024-01-15 05:13:12 +0200

    Add multi-cache support to cache clearing command
    Modify the cache clearing command to support specifying which caches to
    clear or optionally clearing all of them.

--- django/thunderstore/core/management/commands/clear_cache.py
+++ django/thunderstore/core/management/commands/clear_cache.py
@@ -1,14 +1,25 @@
 from django.conf import settings
-from django.core.cache import cache
-from django.core.management.base import BaseCommand, CommandError
+from django.core.cache import caches
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = "Clears all configured caches"
 
+    def add_arguments(self, parser) -> None:
+        parser.add_argument("caches", nargs="*", default=["default"])
+        parser.add_argument("--all", default=False, action="store_true")
+
     def handle(self, *args, **kwargs):
-        if settings.CACHES:
-            cache.clear()
-            self.stdout.write("Caches cleared!")
-        else:
-            raise CommandError("No caches are currently configured")
+        names = kwargs.get("caches", ["default"])
+        if kwargs.get("all") is True:
+            names = settings.CACHES.keys()
+
+        for name in names:
+            if name in settings.CACHES:
+                self.stdout.write(f"Clearing cache: {name}")
+                caches[name].clear()
+            else:
+                self.stderr.write(f"Cache {name} not found, unable to clear")
+
+        self.stdout.write("All done!")
