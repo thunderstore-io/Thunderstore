d87b821bb8d873894ac7ca0c60a98b391217dfa8
753.network <PrimaryInfinity@gmail.com>
2024-10-22 16:11:35 -0400

    Add review_status to PackageVersion
    Add review_status to PackageVersion
    Add reject and approve version actions to Django admin
    Add update_visibility method to PackageVersion

--- django/thunderstore/repository/consts.py
+++ django/thunderstore/repository/consts.py
@@ -1,9 +1,18 @@
 import re
 
+from thunderstore.core.utils import ChoiceEnum
+
 PACKAGE_NAME_REGEX = re.compile(r"^[a-zA-Z0-9\_]+$")
 PACKAGE_VERSION_REGEX = re.compile(r"^[0-9]+\.[0-9]+\.[0-9]+$")
-
 
 PACKAGE_REFERENCE_COMPONENT_REGEX = re.compile(
     r"^[a-zA-Z0-9]+([a-zA-Z0-9\_]+[a-zA-Z0-9])?$"
 )
+
+
+class PackageVersionReviewStatus(ChoiceEnum):
+    pending = "pending"
+    approved = "approved"
+    rejected = "rejected"
+    skipped = "skipped"
+    immune = "immune"
