fd7dd2a511927814d2147e8e41bbe9c554ae7c63
753.network <PrimaryInfinity@gmail.com>
2024-10-22 16:50:55 -0400

    Add migrations to populate visibility
    Add migrations to populate visibility field for PackageVersion and
    PackageListing

---
+++ django/thunderstore/repository/migrations/0056_create_default_visibility_for_existing_versions.py
@@ -0,0 +1,52 @@
+from django.db import migrations
+
+from thunderstore.repository.consts import PackageVersionReviewStatus
+
+
+def create_default_visibility_for_existing_records(apps, schema_editor):
+    PackageVersion = apps.get_model("repository", "PackageVersion")
+    VisibilityFlags = apps.get_model("permissions", "VisibilityFlags")
+
+    for instance in PackageVersion.objects.filter(visibility__isnull=True):
+        visibility_flags = VisibilityFlags.objects.create(
+            public_list=False,
+            public_detail=False,
+            owner_list=True,
+            owner_detail=True,
+            moderator_list=True,
+            moderator_detail=True,
+            admin_list=True,
+            admin_detail=True,
+        )
+        instance.visibility = visibility_flags
+        instance.save()
+        update_visibility(instance)
+
+
+def update_visibility(version):
+    version.visibility.public_detail = True
+    version.visibility.public_list = True
+    version.visibility.owner_detail = True
+    version.visibility.owner_list = True
+    version.visibility.moderator_detail = True
+    version.visibility.moderator_list = True
+
+    if (
+        version.review_status == PackageVersionReviewStatus.rejected
+        or version.review_status == PackageVersionReviewStatus.pending
+    ):
+        version.visibility.public_detail = False
+        version.visibility.public_list = False
+
+    version.visibility.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("repository", "0055_packageversion_review_status"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_default_visibility_for_existing_records),
+    ]
