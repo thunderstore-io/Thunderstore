a136d68d289c22e6492d52c36cf11eb25324d588
753.network <PrimaryInfinity@gmail.com>
2025-02-03 09:13:23 -0500

    Update test_migrations
    Remove redundant MigrationRecorder check from migrate_db_state
    Move migrate_db_state to conftest.py
    Migrate to old migration before creating test data

--- django/thunderstore/core/tests/test_migrations.py
+++ django/thunderstore/core/tests/test_migrations.py
@@ -1,28 +1,14 @@
 import pytest
-from django.db import connection
-from django.db.migrations.executor import MigrationExecutor
-from django.db.migrations.recorder import MigrationRecorder
 
+from conftest import migrate_db_state
 from thunderstore.repository.factories import NamespaceFactory, TeamFactory
 from thunderstore.repository.models import Namespace
 
 
-@pytest.fixture
-def migrate_db_state():
-    def _run_migration(app_name: str, migration: str):
-        executor = MigrationExecutor(connection)
-        executor.migrate([(app_name, migration)])
-        executor.loader.build_graph()
-
-        assert MigrationRecorder.Migration.objects.filter(
-            app=app_name, name=migration
-        ).exists()
-
-    return _run_migration
-
-
 @pytest.mark.django_db
 def test_0055_delete_namespaces_without_team(migrate_db_state):
+    migrate_db_state("repository", "0054_alter_chunked_package_cache_index")
+
     team = TeamFactory.create()
     namespace_with_team = NamespaceFactory.create(team=team)
     namespace_without_team = NamespaceFactory.create(team=None)
@@ -30,7 +16,6 @@ def test_0055_delete_namespaces_without_team(migrate_db_state):
     assert namespace_without_team.team is None
     assert Namespace.objects.count() == 2
 
-    migrate_db_state("repository", "0054_alter_chunked_package_cache_index")
     migrate_db_state("repository", "0055_delete_namespaces_without_team")
 
     assert Namespace.objects.count() == 1
