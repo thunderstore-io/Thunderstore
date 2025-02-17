b8d149faa2bf01dd0800965535847c1124c45ac0
Daniel Vainio <daniel.vainio@protonmail.com>
2025-02-17 15:47:19 +0200

    Implement TeamCreateAPIView
    Implement CreateAPIView for creating new teams in the Cyberstorm API.
    
    Refs. TS-2312

--- django/thunderstore/api/cyberstorm/serializers/__init__.py
+++ django/thunderstore/api/cyberstorm/serializers/__init__.py
@@ -5,12 +5,18 @@ from .community import (
 )
 from .package import CyberstormPackagePreviewSerializer, PackagePermissionsSerializer
 from .team import (
+    CyberstormCreateTeamSerializer,
     CyberstormServiceAccountSerializer,
+    CyberstormTeamAddMemberRequestSerialiazer,
+    CyberstormTeamAddMemberResponseSerialiazer,
     CyberstormTeamMemberSerializer,
     CyberstormTeamSerializer,
 )
 
 __all__ = [
+    "CyberstormTeamAddMemberRequestSerialiazer",
+    "CyberstormTeamAddMemberResponseSerialiazer",
+    "CyberstormCreateTeamSerializer",
     "CyberstormCommunitySerializer",
     "CyberstormPackageCategorySerializer",
     "CyberstormPackageListingSectionSerializer",
