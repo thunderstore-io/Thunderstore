d1f109885fcae6c8fed7a2e11fe813f6bafe17df
Daniel Vainio <daniel.vainio@protonmail.com>
2025-02-18 11:00:10 +0200

    Update DeleteServiceAccountAPIView to use delete
    - Inherit DestroyAPIView in DeleteServiceAccountAPIView.
    - Use delete() instead of post() for deleting service accounts.
    - Add uuid to URL instead of sending a payload to the DELETE endpoint.
    - Update tests accordingly.
    
    Refs. TS-2320

--- django/thunderstore/api/cyberstorm/serializers/service_account.py
+++ django/thunderstore/api/cyberstorm/serializers/service_account.py
@@ -5,7 +5,3 @@ class CreateServiceAccountSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=32)
     team_name = serializers.CharField(read_only=True)
     api_token = serializers.CharField(read_only=True)
-
-
-class DeleteServiceAccountSerializer(serializers.Serializer):
-    uuid = serializers.UUIDField()
