fc37d25701064b1867977f86535a8f29972f8729
753.network <PrimaryInfinity@gmail.com>
2025-05-21 11:14:39 -0400

    Add ts_reports app
    Add ts_reports app
    Add PackageReport model
    Add PackageReport admin

---
+++ django/thunderstore/ts_reports/models/package_report.py
@@ -0,0 +1,88 @@
+from typing import Optional
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from thunderstore.community.models import PackageListing
+from thunderstore.core.mixins import TimestampMixin
+from thunderstore.core.types import UserType
+from thunderstore.repository.models import Package, PackageVersion
+from thunderstore.ts_reports.models._common import ActiveManager
+
+
+class PackageReport(TimestampMixin):
+    objects = ActiveManager()
+
+    is_active = models.BooleanField(default=True)
+
+    category = models.CharField(max_length=255)
+    reason = models.CharField(max_length=255)
+
+    submitted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="package_reports",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    is_automated = models.BooleanField(default=True)
+
+    package = models.ForeignKey(
+        "repository.Package",
+        related_name="reports",
+        on_delete=models.CASCADE,
+    )
+    package_listing = models.ForeignKey(
+        "community.PackageListing",
+        related_name="reports",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    package_version = models.ForeignKey(
+        "repository.PackageVersion",
+        related_name="reports",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.category} : {self.reason}"
+
+    @classmethod
+    def handle_user_report(
+        cls,
+        reason: str,
+        submitted_by: UserType,
+        package: Package,
+        package_listing: PackageListing,
+        package_version: PackageVersion,
+        description: Optional[str],
+    ) -> "PackageReport":
+        return cls.objects.create(
+            category="UserReport",
+            reason=reason,
+            submitted_by=submitted_by,
+            package=package,
+            package_listing=package_listing,
+            package_version=package_version,
+            description=description,
+            is_automated=False,
+        )
+
+    def validate(self):
+        if self.package_listing:
+            if self.package_listing.package != self.package:
+                raise ValidationError("Package mismatch!")
+        if self.package_version:
+            if self.package_version.package != self.package:
+                raise ValidationError("Package mismatch!")
+
+    def save(self, *args, **kwargs):
+        self.validate()
+        return super().save(*args, **kwargs)
