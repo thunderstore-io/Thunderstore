a28159f9e01d7fd9c8fcb7dd92d1e99f4f34200f
753.network <PrimaryInfinity@gmail.com>
2025-06-16 02:56:24 -0400

    Use UserMeta in moderated_communities
    Use UserMeta in moderated_communities for performance

--- django/thunderstore/account/models/user.py
+++ django/thunderstore/account/models/user.py
@@ -3,17 +3,42 @@ from django.contrib.auth.models import AnonymousUser
 
 User = get_user_model()
 
+
+def _get_moderated_communities(user):
+    from thunderstore.repository.views.package._utils import get_moderated_communities
+
+    return get_moderated_communities(user)
+
+
 if not hasattr(User, "moderated_communities"):
 
     def moderated_communities(self):
-        if not hasattr(self, "_moderated_communities"):
-            from thunderstore.repository.views.package._utils import (
-                get_moderated_communities,
-            )
+        if hasattr(self, "_moderated_communities"):
+            return self._moderated_communities
 
-            print("Should run once ever")
-            self._moderated_communities = get_moderated_communities(self)
-        return self._moderated_communities
+        if not self.is_authenticated:
+            self._moderated_communities = []
+            return self._moderated_communities
+
+        if self.is_staff or self.is_superuser:
+            self._moderated_communities = _get_moderated_communities(self)
+            return self._moderated_communities
+
+        from thunderstore.account.models import UserMeta
+
+        try:
+            user_meta = UserMeta.objects.only("can_moderate_any_community").get(
+                user=self
+            )
+            if user_meta.can_moderate_any_community:
+                communities = _get_moderated_communities(self)
+            else:
+                communities = []
+        except UserMeta.DoesNotExist:
+            communities = []
+
+        self._moderated_communities = communities
+        return communities
 
     User.moderated_communities = property(moderated_communities)
 
