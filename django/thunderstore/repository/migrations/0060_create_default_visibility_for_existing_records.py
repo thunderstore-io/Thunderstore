206d3990f59b34316e481c75f1ee01be5558e5b7
Mythic <mythic@thunderstore.io>
2025-06-18 19:11:06 +0300

    Fix and adjust migrations
    Convert the thunderstore.repository 0060 data migration into a noop
    stub, as it wasn't actually required.
    
    Rename thunderstore.repository 0061 to be more informative rather than
    the autogenerated name and add data-update for `pending` -> `unreviewed`
    state change, as to reflect what the migration does for the field.

--- django/thunderstore/repository/migrations/0060_create_default_visibility_for_existing_records.py
+++ django/thunderstore/repository/migrations/0060_create_default_visibility_for_existing_records.py
@@ -1,111 +1,15 @@
 from django.db import migrations
 
-from thunderstore.repository.consts import PackageVersionReviewStatus
+"""
+This migration used to have logic for creating/updating default VisibilityFlags
+for Package/PackageVersion/PackageListing models, but as the system is currently
+not in active use and we have no logic ensuring the state is maintained
+correctly, the data migration was removed as it was considered unnecessary but
+would have made deployment take longer.
 
-
-def create_default_visibility_for_existing_records(apps, schema_editor):
-    PackageVersion = apps.get_model("repository", "PackageVersion")
-    Package = apps.get_model("repository", "Package")
-    VisibilityFlags = apps.get_model("permissions", "VisibilityFlags")
-
-    for instance in PackageVersion.objects.filter(visibility__isnull=True):
-        visibility_flags = VisibilityFlags.objects.create(
-            public_list=False,
-            public_detail=False,
-            owner_list=True,
-            owner_detail=True,
-            moderator_list=True,
-            moderator_detail=True,
-            admin_list=True,
-            admin_detail=True,
-        )
-        instance.visibility = visibility_flags
-        instance.save()
-        update_version_visibility(instance)
-
-    for instance in Package.objects.filter(visibility__isnull=True):
-        visibility_flags = VisibilityFlags.objects.create(
-            public_list=False,
-            public_detail=False,
-            owner_list=True,
-            owner_detail=True,
-            moderator_list=True,
-            moderator_detail=True,
-            admin_list=True,
-            admin_detail=True,
-        )
-        instance.visibility = visibility_flags
-        instance.save()
-        update_package_visibility(instance)
-
-
-def update_version_visibility(version):
-    version.visibility.public_detail = True
-    version.visibility.public_list = True
-    version.visibility.owner_detail = True
-    version.visibility.owner_list = True
-    version.visibility.moderator_detail = True
-    version.visibility.moderator_list = True
-
-    if not version.is_active or not version.package.is_active:
-        version.visibility.public_detail = False
-        version.visibility.public_list = False
-        version.visibility.owner_detail = False
-        version.visibility.owner_list = False
-        version.visibility.moderator_detail = False
-        version.visibility.moderator_list = False
-
-    if version.review_status == PackageVersionReviewStatus.rejected:
-        version.visibility.public_detail = False
-        version.visibility.public_list = False
-
-    version.visibility.save()
-
-
-def update_package_visibility(package):
-    package.visibility.public_detail = True
-    package.visibility.public_list = True
-    package.visibility.owner_detail = True
-    package.visibility.owner_list = True
-    package.visibility.moderator_detail = True
-    package.visibility.moderator_list = True
-
-    if not package.is_active:
-        package.visibility.public_detail = False
-        package.visibility.public_list = False
-        package.visibility.owner_detail = False
-        package.visibility.owner_list = False
-        package.visibility.moderator_detail = False
-        package.visibility.moderator_list = False
-
-    visibility_fields = [
-        "public_detail",
-        "public_list",
-        "owner_detail",
-        "owner_list",
-        "moderator_detail",
-        "moderator_list",
-    ]
-
-    versions = list(
-        package.versions.filter(is_active=True).values(
-            *[f"visibility__{field}" for field in visibility_fields]
-        )
-    )
-
-    any_version_visible = {field: False for field in visibility_fields}
-
-    for field in visibility_fields:
-        for version in versions:
-            if version[f"visibility__{field}"]:
-                any_version_visible[field] = True
-                break
-
-    for field, exists in any_version_visible.items():
-        if not exists:
-            setattr(package.visibility, field, False)
-
-    package.visibility.save()
+Instead, this stub was left around as to not brick migration history for
+environments that might have already run the migration.
+"""
 
 
 class Migration(migrations.Migration):
@@ -115,7 +19,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(
-            create_default_visibility_for_existing_records, migrations.RunPython.noop
-        ),
+        migrations.RunPython(migrations.RunPython.noop, migrations.RunPython.noop),
     ]
---
+++ django/thunderstore/repository/migrations/0061_alter_packageversion_review.py
@@ -0,0 +1,51 @@
+from typing import Iterable, List, TypeVar
+
+from django.db import migrations, models
+
+T = TypeVar("T")
+
+
+def batch(batch_size: int, iterable: Iterable[T]) -> Iterable[List[T]]:
+    collected = []
+    for entry in iterable:
+        collected.append(entry)
+        if len(collected) >= batch_size:
+            yield collected
+            collected = []
+    if len(collected) > 0:
+        yield collected
+
+
+def update_pending_to_unreviewed(apps, schema_editor):
+    PackageVersion = apps.get_model("repository", "PackageVersion")
+
+    version_ids = (
+        PackageVersion.objects.filter(review_status="pending")
+        .values_list("id", flat=True)
+        .iterator()
+    )
+    for ids in batch(2000, version_ids):
+        PackageVersion.objects.filter(id__in=ids).update(review_status="unreviewed")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("repository", "0060_create_default_visibility_for_existing_records"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="packageversion",
+            name="review_status",
+            field=models.TextField(
+                choices=[
+                    ("unreviewed", "unreviewed"),
+                    ("approved", "approved"),
+                    ("rejected", "rejected"),
+                ],
+                default="unreviewed",
+            ),
+        ),
+        migrations.RunPython(update_pending_to_unreviewed, migrations.RunPython.noop),
+    ]
