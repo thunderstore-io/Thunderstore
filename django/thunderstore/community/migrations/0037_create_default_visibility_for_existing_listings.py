00c34293b52343c8d44f7df18323cf712840f358
Mythic <mythic@thunderstore.io>
2025-06-19 10:37:29 +0300

    Convert community 0037 migration to a stub
    Convert the community's 0037 migration to a stub, as it was crashing in
    QA and not actually required yet

--- django/thunderstore/community/migrations/0037_create_default_visibility_for_existing_listings.py
+++ django/thunderstore/community/migrations/0037_create_default_visibility_for_existing_listings.py
@@ -1,63 +1,23 @@
 from django.db import migrations
 
-from thunderstore.community.consts import PackageListingReviewStatus
+"""
+This migration used to have logic for creating/updating default VisibilityFlags
+for Package/PackageVersion/PackageListing models, but as the system is currently
+not in active use and we have no logic ensuring the state is maintained
+correctly, the data migration was removed as it was considered unnecessary but
+would have made deployment take longer.
 
-
-def create_default_visibility_for_existing_records(apps, schema_editor):
-    PackageListing = apps.get_model("community", "PackageListing")
-    VisibilityFlags = apps.get_model("permissions", "VisibilityFlags")
-
-    for instance in PackageListing.objects.filter(visibility__isnull=True):
-        visibility_flags = VisibilityFlags.objects.create(
-            public_list=False,
-            public_detail=False,
-            owner_list=True,
-            owner_detail=True,
-            moderator_list=True,
-            moderator_detail=True,
-            admin_list=True,
-            admin_detail=True,
-        )
-        instance.visibility = visibility_flags
-        instance.save()
-        update_visibility(instance)
-
-
-def update_visibility(listing):
-    package = listing.package
-    listing.visibility.public_detail = package.visibility.public_detail
-    listing.visibility.public_list = package.visibility.public_list
-    listing.visibility.owner_detail = package.visibility.owner_detail
-    listing.visibility.owner_list = package.visibility.owner_list
-    listing.visibility.moderator_detail = package.visibility.moderator_detail
-    listing.visibility.moderator_list = package.visibility.moderator_list
-    listing.visibility.admin_detail = package.visibility.admin_detail
-    listing.visibility.admin_list = package.visibility.admin_list
-
-    if listing.review_status == PackageListingReviewStatus.rejected:
-        listing.visibility.public_detail = False
-        listing.visibility.public_list = False
-
-    if (
-        listing.community.require_package_listing_approval
-        and listing.review_status == PackageListingReviewStatus.unreviewed
-    ):
-        listing.visibility.public_detail = False
-        listing.visibility.public_list = False
-
-    listing.visibility.save()
+Instead, this stub was left around as to not brick migration history for
+environments that might have already run the migration.
+"""
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ("community", "0036_packagelisting_visibility"),
-        ("repository", "0059_package_visibility"),
-        ("repository", "0060_create_default_visibility_for_existing_records"),
     ]
 
     operations = [
-        migrations.RunPython(
-            create_default_visibility_for_existing_records, migrations.RunPython.noop
-        ),
+        migrations.RunPython(migrations.RunPython.noop, migrations.RunPython.noop),
     ]
