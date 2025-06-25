a7948a91906faba6aaa8f30dc0ec86f809ff795f
753.network <PrimaryInfinity@gmail.com>
2025-06-25 16:52:11 -0400

    Increase test coverage for user.moderated_communities

--- django/thunderstore/account/tests/test_user.py
+++ django/thunderstore/account/tests/test_user.py
@@ -5,13 +5,17 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
 
 from thunderstore.account.models import UserMeta
+from thunderstore.community.models import (
+    Community,
+    CommunityMemberRole,
+    CommunityMembership,
+)
 
 User = get_user_model()
 
 
 @pytest.mark.django_db
-def test_user_moderated_communities_only_called_once():
-    user = User.objects.create_user(username="tester", password="pass")
+def test_user_moderated_communities_only_called_once(user: User):
     UserMeta.objects.create(user=user, can_moderate_any_community=True)
 
     with patch(
@@ -29,8 +33,7 @@ def test_user_moderated_communities_only_called_once():
 
 
 @pytest.mark.django_db
-def test_user_has_moderated_communities_is_empty_list_by_default():
-    user = User()
+def test_user_has_moderated_communities_is_empty_list_by_default(user: User):
     assert hasattr(user, "moderated_communities")
     assert user.moderated_communities == []
 
@@ -41,3 +44,33 @@ def test_anonymous_user_moderated_communities_is_empty_list():
     assert anon.is_authenticated == False
     assert hasattr(anon, "moderated_communities")
     assert anon.moderated_communities == []
+
+
+@pytest.mark.django_db
+def test_user_moderated_communities_with_user_meta_and_can_moderate(
+    user: User, community: Community
+):
+    CommunityMembership.objects.create(
+        user=user, community=community, role=CommunityMemberRole.moderator
+    )
+
+    user_meta = UserMeta.objects.get(user=user)
+    assert user_meta.can_moderate_any_community == True
+
+    assert hasattr(user, "moderated_communities")
+    assert user.moderated_communities == [str(community.id)]
+
+
+@pytest.mark.django_db
+def test_user_moderated_communities_with_user_meta_and_can_not_moderate(
+    user: User, community: Community
+):
+    CommunityMembership.objects.create(
+        user=user, community=community, role=CommunityMemberRole.member
+    )
+
+    user_meta = UserMeta.objects.get(user=user)
+    assert user_meta.can_moderate_any_community == False
+
+    assert hasattr(user, "moderated_communities")
+    assert user.moderated_communities == []
