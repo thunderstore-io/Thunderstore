33cf9408ec720c6e9b5633bb299a35e43ed0601f
753.network <PrimaryInfinity@gmail.com>
2025-09-01 14:13:40 -0400

    Update markdown tests
    Update markdown tests to account for user-content slug change

--- django/thunderstore/api/cyberstorm/tests/test_markdown.py
+++ django/thunderstore/api/cyberstorm/tests/test_markdown.py
@@ -67,9 +67,7 @@ def test_readme_api_view__prerenders_markup(api_client: APIClient) -> None:
         f"/api/cyberstorm/package/{v.package.namespace}/{v.package.name}/latest/readme/",
     )
     actual = response.json()
-    expected_html = (
-        '<h1 id="very-strong-header">Very <strong>strong</strong> header</h1>\n'
-    )
+    expected_html = '<h1 id="user-content-very-strong-header">Very <strong>strong</strong> header</h1>\n'
     assert actual["html"] == expected_html
 
 
