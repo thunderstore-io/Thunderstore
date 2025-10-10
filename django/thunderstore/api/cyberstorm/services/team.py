c8ba27d7a29c9ffd22858bf38760b0b91ed537e3
Daniel Vainio <daniel.vainio@protonmail.com>
2025-10-10 12:44:41 +0300

    Update team services
    Update disband_team service and create_team service. Utilize
    Team.create() in create_team service and remove redundant validation
    errors. Use objects as parameters in the services instead of trying to
    fetch and raise 404 errors in services.
    
    Refs. TS-2426

--- django/thunderstore/api/cyberstorm/services/team.py
+++ django/thunderstore/api/cyberstorm/services/team.py
@@ -1,36 +1,28 @@
-from django.core.exceptions import ValidationError
 from django.db import transaction
-from django.shortcuts import get_object_or_404
 
 from thunderstore.account.models import ServiceAccount
 from thunderstore.core.exceptions import PermissionValidationError
 from thunderstore.core.types import UserType
-from thunderstore.repository.models import Namespace, Team, TeamMember
+from thunderstore.repository.models import Team, TeamMember
 from thunderstore.repository.models.team import TeamMemberRole
 
 
 @transaction.atomic
-def disband_team(user: UserType, team_name: str) -> None:
-    teams = Team.objects.exclude(is_active=False)
-    team = get_object_or_404(teams, name=team_name)
-    team.ensure_user_can_access(user)
-    team.ensure_user_can_disband(user)
+def disband_team(agent: UserType, team: Team) -> None:
+    team.ensure_user_can_access(agent)
+    team.ensure_user_can_disband(agent)
     team.delete()
 
 
 @transaction.atomic
-def create_team(user: UserType, team_name: str) -> Team:
-    if not user or not user.is_authenticated or not user.is_active:
+def create_team(agent: UserType, team_name: str) -> Team:
+    if not agent or not agent.is_authenticated or not agent.is_active:
         raise PermissionValidationError("Must be authenticated to create teams")
-    if getattr(user, "service_account", None) is not None:
+    if getattr(agent, "service_account", None) is not None:
         raise PermissionValidationError("Service accounts cannot create teams")
-    if Team.objects.filter(name=team_name).exists():
-        raise ValidationError("A team with the provided name already exists")
-    if Namespace.objects.filter(name=team_name).exists():
-        raise ValidationError("A namespace with the provided name already exists")
 
-    team = Team.objects.create(name=team_name)
-    team.add_member(user=user, role=TeamMemberRole.owner)
+    team = Team.create(name=team_name)
+    team.add_member(user=agent, role=TeamMemberRole.owner)
     return team
 
 
