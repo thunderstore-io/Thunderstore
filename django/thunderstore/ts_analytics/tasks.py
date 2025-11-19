b039a621f93aeb5b5530c484e1185f990dede61d
Mythic <mythic@thunderstore.io>
2025-11-19 14:23:29 +0200

    Remove unused variable
    Remove an unused variable that was accidentally left over when
    refactoring

--- django/thunderstore/ts_analytics/tasks.py
+++ django/thunderstore/ts_analytics/tasks.py
@@ -5,12 +5,10 @@ from celery import shared_task
 from thunderstore.core.settings import CeleryQueues
 from thunderstore.ts_analytics.kafka import get_kafka_client
 
-TASK_SEND_KAFKA_MESSAGE = "thunderstore.ts_analytics.tasks.send_kafka_message"
-
 
 @shared_task(
     queue=CeleryQueues.Analytics,
-    name="thunderstore.analytics.send_kafka_message",
+    name="thunderstore.ts_analytics.tasks.send_kafka_message",
     ignore_result=True,
 )
 def send_kafka_message(topic: str, payload_string: str, key: Optional[str] = None):
