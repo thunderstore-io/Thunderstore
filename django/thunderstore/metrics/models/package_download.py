f68a78e731f437b5fb398b957de19874fec832de
Andy Brenneke <andy.brenneke@gmail.com>
2026-01-03 15:35:04 -0800

    add BRIN index to PackageVersionDownloadEvent.timestamp

--- django/thunderstore/metrics/models/package_download.py
+++ django/thunderstore/metrics/models/package_download.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.indexes import BrinIndex
 from django.db import models
 
 
@@ -5,3 +6,11 @@ class PackageVersionDownloadEvent(models.Model):
     id = models.BigAutoField(primary_key=True, editable=False)
     version_id = models.BigIntegerField(db_index=True)
     timestamp = models.DateTimeField()
+    
+    class Meta:
+        indexes = [
+            # A BRIN index is perfect here - it is 100x-1000x smaller than a b-tree and much faster
+            # to create and insert. This table is ~20M rows right now, so saves a lot of space.
+            # This table is insert-only, and filtering happens by timestamp ranges.
+            BrinIndex(fields=["timestamp"]),
+        ]
