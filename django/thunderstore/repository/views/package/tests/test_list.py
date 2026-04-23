cc7c380c29c9377338e01366e7ed12ad61029261
753.network <PrimaryInfinity@gmail.com>
2026-04-23 11:46:01 -0400

    Add category filtering tests to satisfy codecov
    Add category filtering tests to satisfy codecov

--- django/thunderstore/repository/views/package/tests/test_list.py
+++ django/thunderstore/repository/views/package/tests/test_list.py
@@ -5,8 +5,13 @@ from django.urls import reverse
 from thunderstore.cache.enums import CacheBustCondition
 from thunderstore.cache.tasks import invalidate_cache
 from thunderstore.community.consts import PackageListingReviewStatus
+from thunderstore.community.factories import (
+    PackageCategoryFactory,
+    PackageListingFactory,
+)
 from thunderstore.community.models import CommunitySite, PackageListing
 from thunderstore.core.types import UserType
+from thunderstore.repository.factories import PackageVersionFactory
 
 
 @pytest.mark.django_db
@@ -36,6 +41,37 @@ def test_package_review_list_view(
     response = client.get(url, HTTP_HOST=community_site.site.domain)
     assert response.status_code == 200
     assert active_package_listing.get_full_url().encode("utf-8") in response.content
+
+
+@pytest.mark.django_db
+def test_package_list_search_logic(
+    client: Client,
+    community_site: CommunitySite,
+):
+    url = reverse(
+        "communities:community:packages.list",
+        kwargs={
+            "community_identifier": community_site.community.identifier,
+        },
+    )
+    host = community_site.site.domain
+
+    version = PackageVersionFactory(name="Fish_and_Birds")
+    listing = PackageListingFactory(
+        package=version.package, community=community_site.community
+    )
+
+    resp = client.get(f"{url}?q= ", HTTP_HOST=host)
+    assert listing.get_full_url().encode("utf-8") in resp.content
+
+    resp = client.get(f"{url}?q=fish", HTTP_HOST=host)
+    assert listing.get_full_url().encode("utf-8") in resp.content
+
+    resp = client.get(f"{url}?q=fish birds", HTTP_HOST=host)
+    assert listing.get_full_url().encode("utf-8") in resp.content
+
+    resp = client.get(f"{url}?q=fish cats", HTTP_HOST=host)
+    assert listing.get_full_url().encode("utf-8") not in resp.content
 
 
 @pytest.mark.django_db
@@ -75,3 +111,55 @@ def test_package_list_cache_vary_sanitization(community_site: CommunitySite):
         "q:search"
     )
     assert cache_vary == expected_order
+
+
+@pytest.mark.django_db
+def test_package_list_category_filtering(
+    client: Client,
+    community_site: CommunitySite,
+    active_package_listing: PackageListing,
+):
+    cat1 = PackageCategoryFactory()
+    cat2 = PackageCategoryFactory()
+
+    active_package_listing.categories.add(cat1)
+
+    url = reverse(
+        "communities:community:packages.list",
+        kwargs={
+            "community_identifier": community_site.community.identifier,
+        },
+    )
+
+    resp = client.get(
+        f"{url}?included_categories={cat1.pk}&included_categories={cat2.pk}",
+        HTTP_HOST=community_site.site.domain,
+    )
+    assert resp.status_code == 200
+    # Package has one of the included categories, search uses OR logic for categories
+    assert active_package_listing.get_full_url().encode("utf-8") in resp.content
+
+    resp = client.get(
+        f"{url}?excluded_categories={cat1.pk}", HTTP_HOST=community_site.site.domain
+    )
+    assert resp.status_code == 200
+    # Package has one of the excluded categories, search uses OR logic for categories
+    assert active_package_listing.get_full_url().encode("utf-8") not in resp.content
+
+
+@pytest.mark.django_db
+def test_package_list_empty_category_params(
+    client: Client,
+    community_site: CommunitySite,
+):
+    url = reverse(
+        "communities:community:packages.list",
+        kwargs={
+            "community_identifier": community_site.community.identifier,
+        },
+    )
+    resp = client.get(
+        f"{url}?included_categories=arbitrarystring",
+        HTTP_HOST=community_site.site.domain,
+    )
+    assert resp.status_code == 200
