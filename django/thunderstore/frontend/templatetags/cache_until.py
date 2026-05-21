1acf954e58af68fad5e610aa3f0293d6ffaa1e63
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2026-05-21 21:44:05 +0200

    feat: add debug cache ttl for html generation

--- django/thunderstore/frontend/templatetags/cache_until.py
+++ django/thunderstore/frontend/templatetags/cache_until.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.template import Library, Node, TemplateSyntaxError, VariableDoesNotExist
 
 from thunderstore.cache.cache import (
@@ -39,6 +40,13 @@ class CacheNode(Node):
                 raise TemplateSyntaxError(
                     f'"cache_until" tag got a non-integer expiry value: {expire_time}'
                 )
+
+        if (
+            expire_time is not None
+            and settings.DEBUG
+            and settings.DEBUG_CACHE_TTL is not None
+        ):
+            expire_time = settings.DEBUG_CACHE_TTL
 
         vary_on = [var.resolve(context) for var in self.vary_on]
 
