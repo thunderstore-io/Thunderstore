742db7e3fa53f5ba5ef175685e9542ffd4a9d9b5
Ethan Green <ethangreen.dev@gmail.com>
2026-09-23 17:44:57 +0300

    Serve README and CHANGELOG overrides from the read endpoints

--- django/thunderstore/repository/api/experimental/serializers/markdown.py
+++ django/thunderstore/repository/api/experimental/serializers/markdown.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 
 class MarkdownResponseSerializer(serializers.Serializer):
     markdown = serializers.CharField(required=True, allow_null=True)
+    is_edited = serializers.BooleanField(default=False)
+    edited_at = serializers.DateTimeField(allow_null=True, default=None)
